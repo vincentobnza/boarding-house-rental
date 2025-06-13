1921164200621e0396c7f12176229d87322fe089
Vincent#05 <vincentobenza2003@gmail.com>
2025-06-13 20:21:55 +0800

    Added a listing page for landlord

--- client/src/layout/landlord-layout.tsx
+++ client/src/layout/landlord-layout.tsx
@@ -1,13 +1,64 @@
-import { Outlet } from "react-router-dom";
-import Sidebar from "../pages/landlord/sidebar";
+import { NavLink, Outlet } from "react-router-dom";
+import { House, Mail, Ellipsis } from "lucide-react";
+import Footer from "@/components/footer";
 
 export default function LandlordLayout() {
   return (
-    <div className="w-full flex min-h-screen">
-      <Sidebar />
+    <div className="w-full flex flex-col min-h-screen">
+      <nav>
+        <header className="w-full h-18 bg-zinc-50 border-b border-zinc-200 flex justify-between items-center px-9">
+          <h3 className="text-sm">SMART SEARCH</h3>
+          <h1 className="text-2xl font-bold">RENTAL OWNER</h1>
+          <div>Owner</div>
+        </header>
+        <Nav />
+      </nav>
       <main className="flex-1 w-full max-w-screen-2xl mx-auto p-9 flex flex-col justify-center items-center">
         <Outlet />
       </main>
+
+      <Footer />
     </div>
   );
 }
+
+const Nav = () => {
+  const navItems = [
+    {
+      name: "Listings",
+      link: "/landlord/dashboard",
+      icon: <House className="size-4" />,
+    },
+    {
+      name: "Inbox",
+      link: "/landlord/dashboard/inbox",
+      icon: <Mail className="size-4" />,
+    },
+    {
+      name: "Menu",
+      link: "/landlord/dashboard/menu",
+      icon: <Ellipsis className="size-4" />,
+    },
+  ];
+  return (
+    <div className="w-full p-1 border-b border-zinc-200 flex justify-center items-center px-9 gap-6">
+      {navItems.map((item) => (
+        <NavLink
+          key={item.name}
+          to={item.link}
+          className={({ isActive }) =>
+            [
+              "p-5 flex justify-center items-center gap-2 text-sm text-zinc-600 hover:text-zinc-900",
+              isActive
+                ? "text-zinc-900 font-semibold border-b-2 border-zinc-900"
+                : "",
+            ].join(" ")
+          }
+        >
+          {item.icon}
+          <span className="text-lg">{item.name}</span>
+        </NavLink>
+      ))}
+    </div>
+  );
+};
--- client/src/pages/landlord/dashboard.tsx
+++ client/src/pages/landlord/dashboard.tsx
@@ -1,7 +1,33 @@
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+
 export default function Dashboard() {
+  const isThereAnyListing = false;
   return (
     <div className="w-full">
-      <h1>LISTING</h1>
+      {isThereAnyListing ? (
+        <div className="w-full h-full flex justify-center items-center">
+          <h1 className="text-2xl font-bold">Your Listings</h1>
+        </div>
+      ) : (
+        <div className="w-full h-full flex flex-col justify-center items-center">
+          <img
+            src="https://cdn-icons-png.flaticon.com/128/7486/7486754.png"
+            alt="No Listings"
+            className="size-24 mb-4"
+          />
+          <h1 className="text-md">Ohh no! You don't have any listings yet.</h1>
+
+          <Button
+            className="mt-12 h-10 shadow-none rounded"
+            size="sm"
+            variant="outline"
+          >
+            <Plus />
+            Create Listing
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
