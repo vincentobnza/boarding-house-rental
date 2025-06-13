9e334fc1dc2e65c532af8104c1e018a3d36b9b69
Vincent#05 <vincentobenza2003@gmail.com>
2025-06-13 09:04:49 +0800

    Add loading screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,8 +6,10 @@ import {
 } from "react-router-dom";
 import RootLayout from "./layout/root-layout";
 import { lazy, Suspense } from "react";
+import LoadingScreen from "./components/loading";
 const SplashScreen = lazy(() => import("./pages/user/splash-screen"));
 const Homepage = lazy(() => import("./pages/user/homepage"));
+const LandlordHome = lazy(() => import("./pages/landlord/home"));
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -16,19 +18,14 @@ const router = createBrowserRouter(
       <Route path="/home" element={<RootLayout />}>
         <Route index element={<Homepage />} />
       </Route>
+      <Route path="/landlord" element={<LandlordHome />} />
     </>
   )
 );
 
 export default function App() {
   return (
-    <Suspense
-      fallback={
-        <div className="w-full h-screen flex items-center justify-center">
-          <h1 className="text-2xl text-gray-700">Loading...</h1>
-        </div>
-      }
-    >
+    <Suspense fallback={<LoadingScreen />}>
       <RouterProvider router={router} />
     </Suspense>
   );
---
+++ client/src/pages/landlord/home.tsx
@@ -0,0 +1,33 @@
+import BackgroundImage from "@/assets/homepage_image.jpg";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <div className="w-full min-h-screen bg-zinc-100 flex items-center justify-center relative">
+      <img
+        src={BackgroundImage}
+        alt="Background"
+        className="absolute inset-0 w-full h-full object-cover opacity-20 grayscale"
+      />
+      <div className="w-full max-w-lg mx-auto flex flex-col items-center justify-center gap-8 z-5 p-5">
+        <div className="px-4">
+          <p className="font-bold">LANDLORD</p>
+        </div>
+        <h1 className="text-5xl md:text-7xl font-bold">Smart Search</h1>
+        <p className="text-sm md:text-lg text-center font-semibold mb-12">
+          Web Based Boarding and Rental House Locator Platform using Results
+          Ranking and Filtering (RRF) in San Jose, Occidental Mindoro
+        </p>
+
+        <Button size="lg" asChild className="bg-zinc-900">
+          <Link to="/home?type=user">Login your Account</Link>
+        </Button>
+        <p>OR</p>
+        <Button size="lg" variant="link" asChild>
+          <Link to="/home?type=user">Register an Account</Link>
+        </Button>
+      </div>
+    </div>
+  );
+}
