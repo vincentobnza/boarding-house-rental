75c88b9e61ff43f1d7a68f8cc182a266a54e5d2e
Vincent#05 <vincentobenza2003@gmail.com>
2025-06-14 19:58:34 +0800

    Fix routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,74 +1,12 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  createRoutesFromElements,
-  Route,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Suspense } from "react";
 import LoadingScreen from "./components/loading";
-import RootLayout from "./layout/root-layout";
-import { lazy } from "react";
-const SplashScreen = lazy(() => import("./pages/splash-screen"));
-const Homepage = lazy(() => import("./pages/tenant/homepage"));
-
-const LandlordHome = lazy(() => import("./pages/landlord/home"));
-const Onboarding = lazy(() => import("./pages/onboarding"));
-const LandlordLogin = lazy(() => import("./pages/landlord/login"));
-const LandlordSignup = lazy(() => import("./pages/landlord/signup"));
-const LandlordLayout = lazy(() => import("./layout/landlord-layout"));
-
-const LandlordDashboard = lazy(
-  () => import("./pages/landlord/listings/dashboard")
-);
-const LandlordRegistration = lazy(
-  () => import("./pages/landlord/registration")
-);
-const LandlordInbox = lazy(() => import("./pages/landlord/inbox/chat"));
-// Admin pages
-const AdminLayout = lazy(() => import("./layout/admin-layout"));
-const AdminHome = lazy(() => import("./pages/admin/home"));
-const AdminLogin = lazy(() => import("./pages/admin/login"));
-const AdminDashboard = lazy(() => import("./pages/admin/overview/dashboard"));
-const AdminPendingListings = lazy(
-  () => import("./pages/admin/pending-listing/list")
-);
-
-// query client
+import allRoutes from "./routes";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
 import { queryClient } from "./lib/query-client";
-import { Toaster } from "@/components/ui/sonner";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route index element={<SplashScreen />} />
-      <Route path="/onboarding" element={<Onboarding />} />
-      <Route path="/tenant" element={<RootLayout />}>
-        <Route index element={<Homepage />} />
-      </Route>
-      {/* LANDLORD PAGES */}
-      <Route path="/landlord" element={<LandlordHome />} />
-      <Route path="/landlord/login" element={<LandlordLogin />} />
-      <Route path="/landlord/signup" element={<LandlordSignup />} />
-      <Route path="/landlord/registration" element={<LandlordRegistration />} />
-
-      {/* LANDLORD DASHBOARD */}
-      <Route path="/landlord/dashboard" element={<LandlordLayout />}>
-        <Route path="listings" element={<LandlordDashboard />} />
-        <Route path="inbox" element={<LandlordInbox />} />
-      </Route>
-
-      {/* ADMIN PAGES */}
-      <Route path="/admin" element={<AdminHome />} />
-      <Route path="/admin/login" element={<AdminLogin />} />
-
-      <Route path="/admin/dashboard" element={<AdminLayout />}>
-        <Route path="dashboard" element={<AdminDashboard />} />
-        <Route path="pending-list" element={<AdminPendingListings />} />
-      </Route>
-    </>
-  )
-);
+const router = createBrowserRouter(allRoutes);
 
 export default function App() {
   return (
