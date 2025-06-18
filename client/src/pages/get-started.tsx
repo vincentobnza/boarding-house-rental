73d72f22166a3ca07ff6c368c4dc552038ebb5bc
Vincent#05 <vincentobenza2003@gmail.com>
2025-06-18 17:10:08 +0800

    Added changes

--- client/src/pages/get-started.tsx
+++ client/src/pages/get-started.tsx
@@ -5,8 +5,6 @@ import { Link, useNavigate } from "react-router-dom";
 import { ArrowRight, House, User } from "lucide-react";
 import React from "react";
 
-// GET STARTED PAGE
-
 export default function GetStarted() {
   const userTypes = [
     {
